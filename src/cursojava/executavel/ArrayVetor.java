5d42ed051214fa83330f5bc92e4a9b6e88aa94df
Rafael Mendes <mafaelrendes8080@gmail.com>
2020-06-03 11:34:26 -0300

    Descobrindo a maior nota em um array

--- src/cursojava/executavel/ArrayVetor.java
+++ src/cursojava/executavel/ArrayVetor.java
@@ -1,44 +1,59 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
 
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 		
+		double[] notas = {8.8, 9.7, 7.6, 6.8};
+		double[] notas2 = {7.8, 8.7, 9.6, 6.8};
 		
-		double[] valores = {9.5, 8.9, 5.8};
-		String[] values = new String[4];
-		String[] valuesString = {"teste", "teste2", "teste3", "teste4", "teste5","teste6"};
+		//Criação dos alunos
+		Aluno aluno = new Aluno();
+		aluno.setNome("Alex Fernando Egidio");
+		aluno.setNomeEscola("JDEV Treinamento");
 		
-		values[0] = "Alex";
-		values[1] = "90";
-		values[2] = "Curso de java";
-		values[3] = "contato@jdevtreinamento.com.br";
+		//Criação das disciplins
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso de Java");
 		
-		int[] inteiros = new int[10];
-		float[] floats = new float[10]; 
 		
-		/*Array pode ser de todos os tipos de dados e objetos também*/
 		
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
+		disciplina.setNota(notas);
 		
-		double[] notas = new double[Integer.parseInt(posicoes)];
+		aluno.getDisciplina().add(disciplina);
 		
-		for (int pos = 0; pos < notas.length; pos++) {
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Logica de programação");
+		disciplina2.setNota(notas);
+		
+		aluno.getDisciplina().add(disciplina2);
+		
+		System.out.println("Nome do aluno = " + aluno.getNome() + " inscrito no curso : " + aluno.getNomeEscola());
+		System.out.println(" ------Disciplina do aluno--------- ");
+		
+		for (Disciplina d : aluno.getDisciplina()) {
 			
-			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
-			notas[pos] = Double.valueOf(valor);
+			System.out.println(" Disciplina : " + d.getDisciplina());
+			System.out.println("A notas da disciplina são");
+			
+			double notaMax = 0.0;
+			for (int pos = 0; pos < d.getNota().length; pos++) {
+				System.out.println("Nota " + pos + " é igual = " + d.getNota()[pos]);
+				
+				if (pos == 0) {
+					notaMax = d.getNota()[pos];
+				}else {
+					if (d.getNota()[pos] > notaMax) {
+						notaMax = d.getNota()[pos];
+					}
+				}
+			}
+			System.out.println("A maior nota da Disciplina = " + d.getDisciplina() + " e de valor : " + notaMax);
 			
 		}
-		
-		/*Atribuir valor nas posições dos arrays*/
-
-		
-		for (int i = 0; i < notas.length; i++) {
-			System.out.println("Nota " + (i + 1) + " é = " + notas[i]);
-		}
-		
 		
 	}
 	
